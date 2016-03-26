fafbcddba48e5daca1f69b6d92124e491191b565
Blake <btickell@berkeley.edu>
2016-03-26 03:51:03 -0700

    FInished vertical check, can change implementation if necessary.

--- con4/board.py
+++ con4/board.py
@@ -1,13 +1,10 @@
 """"Connect 4 Game"""
 
-def _create_board(N, M):
-		l = [["-" for i in range(N)] for i in range(M)]
-		return l 
 class GameBoard:
 	"""Holds a list of lists"""
 	pieces = ["-", "r", "b"] #Pieces that are valid: "-" --> Empty slot, "b" --> blue piece, "r" --> red piece
 	def __init__(self, N, M):
-		self.grid = _create_board(N, M)
+		self.grid = [["-" for i in range(M)] for i in range(N)]
 		self._row = N
 		self._col = M
 		self.player1 = "Red"
@@ -22,19 +19,25 @@ class GameBoard:
 		else:
 			self.piece = "r"
 
+	def valid(self, N, M):
+		if N < self._row and N >= 0:
+			if M < self._col and M >= 0:
+				return True 
+		return False
+
 	def is_empty(self, row:int, col:int):
 		return self[row, col] == "-"
 
 	def __getitem__(self, pos:(int, int)):
 		row, col = pos
-		assert 0 <= row < self._row, "GameBoard::get_piece:: Row is out of range!"
-		assert 0 <= col < self._col, "GameBoard::get_piece:: Column is out of range!"
+		assert 0 <= row and row < self._row, "GameBoard::get_piece:: Row is out of range!"
+		assert 0 <= col and row < self._col, "GameBoard::get_piece:: Column is out of range!"
 		return self.grid[row][col]
 
 	def __setitem__(self, pos:(int, int), val):
 		row, col = pos
-		assert 0 <= row < self._row, "GameBoard::get_piece:: Row is out of range!"
-		assert 0 <= col < self._col, "GameBoard::get_piece:: Column is out of range!"
+		assert 0 <= row and row < self._row, "GameBoard::get_piece:: Row is out of range!"
+		assert 0 <= col and row < self._col, "GameBoard::get_piece:: Column is out of range!"
 		self.grid[row][col] = val
  
 	def add_piece(self, target: int):
@@ -46,6 +49,7 @@ class GameBoard:
 			to_place -= 1
 
 		self[to_place, target] = self.piece
+		return to_place, target
 
 
 
--- con4/main.py
+++ con4/main.py
@@ -1,4 +1,5 @@
 from board import *
+from logic import *
 
 def main():
 	row_num = input("Enter the number of rows [default: 6] : ")
@@ -8,7 +9,7 @@ def main():
 	if col_num == "":
 		col_num = 7
 	board = GameBoard(row_num, col_num)
-
+	logic = ConnectLogic(board)
 	not_end = True
 	print(board)
 	while not_end:
@@ -16,8 +17,11 @@ def main():
 			com = input("Enter a command: ")
 			if com == "q":
 				break
-			board.add_piece(eval(com))
+			pos = board.add_piece(eval(com)) # Gives us the most recent position of the piece placed.
 			print(board)
+			i, j = pos
+			if logic.check_vert_win(i, j):
+				print("TEMPORARY CONGRATZ METHOD")
 			board.player_swap()
 		except:
 			pass
