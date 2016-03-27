547d3239b87eb86f94f713f2620d6760ac575520
BTickell <btickell@berkeley.edu>
2016-03-26 22:26:49 -0700

    set up window, started using pygame

--- con4/gui.py
+++ con4/gui.py
@@ -1,8 +1,18 @@
 from main import *
 import pygame
 
+from pygame.locals import *
+
 pygame.init()
 
-new_display = pygame.display.set_mode((300,000))
+new_display = pygame.display.set_mode((300,300))
 
-pygame.display.set_caption('My First Game')
+pygame.display.set_caption('Connect 4')
+
+while True:
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			pygame.quit()
+			sys.exit()
+
+	pygame.display.update()
