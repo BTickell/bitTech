d8957a7e849fba7b9f19e110d1f21e953a0d1cc5
Blake <btickell@berkeley.edu>
2016-03-28 18:10:27 -0700

    restructured folders

--- con4/gui.py
+++ con4/gui.py
@@ -1,18 +1,56 @@
 from main import *
 import pygame
+from board import *
+from logic import *
 
 from pygame.locals import *
 
 pygame.init()
+pygame.font.init()
 
-new_display = pygame.display.set_mode((300,300))
+new_display = pygame.display.set_mode((1193,961))
+
+img = pygame.image.load('graphics/con4.png')
+r_piece = pygame.image.load('graphics/red_piece.png').convert_alpha() #image for pieces
+b_piece = pygame.image.load('graphics/blue_piece.png').convert_alpha()
 
 pygame.display.set_caption('Connect 4')
 
+board = GameBoard(6, 7) #keeps track of pieces and logic behind the screen
+width = 170.4286 #dimensions for each individual slot on the board
+height = 160.17
+grid = [] #array that keeps track of how many pieces are in each column visually
+for i in range(7): #when an element in the grid is 1,it means the gui already displays piece there
+	grid.append(0)
+
+game_over = False
+
+new_display.blit(img, (0,0))
+pygame.display.update()
 while True:
 	for event in pygame.event.get():
+		if event.type == MOUSEBUTTONUP:
+			if game_over:
+				break
+			pos = pygame.mouse.get_pos()
+			col = pos[0] // width #rounds pixel pos clicked to closest actual slot
+			if grid[int(col)] < 6:
+				grid[int(col)] += 1
+				to_place = board.add_piece(int(col)) #lowest non_empty row in clicked column
+				if board.current == 1:
+					new_display.blit(r_piece, (col*width + 27, to_place[0]*height + 30))
+				else:
+					new_display.blit(b_piece, (col*width + 27, to_place[0]*height + 30))
+
+				if check_all_wins(board, to_place[0], to_place[1]):
+					winner = eval("board.player" + str(board.current))
+					font1 = pygame.font.SysFont("comicsansms", 64)
+					game_message = font1.render(winner + " has won!", True, (55,55,0))
+					new_display.blit(game_message, (400,400))
+					game_over = True
+				
+				board.player_swap()
+				
+				pygame.display.flip()
 		if event.type == QUIT:
 			pygame.quit()
-			sys.exit()
-
-	pygame.display.update()
--- con4/logic.py
+++ con4/logic.py
@@ -77,25 +77,3 @@ def check_r_diag(board:GameBoard, i , j):
 
 def check_all_wins(board:GameBoard, i, j):
 	return check_lat_win(board,i,j) or check_long_win(board,i,j) or check_r_diag(board,i,j) or check_l_diag(board,i,j)
-"""
-class ConnectLogic:
-	def __init__(self, board):
-		self.board = board
-	def check_vert_win(self, i, j):
-	#Check if the slot (i, j) corresponds to a win in the vertical direction
-		piece_count = 1
-		board = self.board
-		piece = board.piece
-		pointer = i - 1
-		while board.valid(pointer, j) and board[pointer, j] == piece:
-			piece_count += 1
-			pointer -= 1
-		pointer = i + 1
-		while board.valid(pointer, j) and board[pointer, j] == piece:
-			piece_count += 1
-			pointer += 1
-
-		if piece_count == 4:
-			return True
-		return False
-"""
